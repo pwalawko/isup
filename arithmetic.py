4968f0ecd16eb532a144d79271ecf60791108027
Paulina Walawko <paulina.walawko@stxnext.pl>
2016-02-26 17:40:13 +0100

    possible to import now

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,22 @@
 import sys
 
 
-def counting():
+def counting(a1, r, n):
     try:
-        a1 = int(sys.argv[1])
-        n = int(sys.argv[3])
-        r = int(sys.argv[2])
+        a1 = int(a1)
+        n = int(n)
+        r = int(r)
     except ValueError:
         print('Incorrect parameter!')
+    else:
+        print(a1 + (n - 1) * r)
+
+if __name__ == '__main__':
+    try:
+        a1 = sys.argv[1]
+        n = sys.argv[3]
+        r = sys.argv[2]
     except IndexError:
         print('Parameter missing!')
     else:
-        print(a1 + (n-1) * r)
-
-counting()
+        counting(a1, r, n)
