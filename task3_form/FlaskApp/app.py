143eff55eee3ccc95ae99a58c35fca8e6c44952a
Paulina Gatkowska <paulina.gatkowska@stxnext.pl>
2018-01-09 08:53:09 +0100

    the dictionary changed to a list

--- task3_form/FlaskApp/app.py
+++ task3_form/FlaskApp/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-orders = {}
+orders = []
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -11,7 +11,7 @@ def show_page():
     if request.method == 'POST':
         person = request.form['person']
         food = request.form['food']
-        orders[person] = food
+        orders.append([person, food])
         return redirect("/")
     return render_template('index.html', orders=orders)
 
