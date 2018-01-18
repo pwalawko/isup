a59c5853df564609e2920a02621c10883a910544
Paulina Gatkowska <paulina.gatkowska@stxnext.pl>
2018-01-18 15:36:34 +0100

    improved validation

--- task3_form/FlaskApp/forms.py
+++ task3_form/FlaskApp/forms.py
@@ -4,12 +4,17 @@ from wtforms import Form, StringField, validators, ValidationError
 req_field_msg = "To pole jest wymagane."
 
 
-def length(min=0, max=0):
-    message = 'Pole musi mieć od %d do %d znaków.' % (min, max)
+def length(min=-1, max=-1):
+    if min == -1 and max != -1:
+        message = 'Pole musi mieć maksymalnie %d znaków.' % max
+    elif min != -1 and max == -1:
+        message = 'Pole musi mieć co najmniej %d znaków.' % min
+    else:
+        message = 'Pole musi mieć od %d do %d znaków.' % (min, max)
 
     def _length(form, field):
-        l = field.data and len(field.data) or 0
-        if l < min or max != -1 and l > max:
+        le = field.data and len(field.data) or 0
+        if le < min or max != -1 and le > max:
             raise ValidationError(message)
 
     return _length
@@ -18,9 +23,9 @@ def length(min=0, max=0):
 class OrderForm(Form):
     person = StringField('Zamawiający', [
         validators.InputRequired(req_field_msg),
-        length(min=3, max=50)
+        length(min=3, max=64)
     ])
     food = StringField('Zamówienie', [
         validators.InputRequired(req_field_msg),
-        length(max=50)
+        length(max=256)
     ])
