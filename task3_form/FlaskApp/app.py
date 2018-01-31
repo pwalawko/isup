e68e26b80d65436ccc00a1c69506c22c9b8cc58f
Paulina Gatkowska <paulina.gatkowska@stxnext.pl>
2018-01-31 15:47:18 +0100

    removed wtforms

--- task3_form/FlaskApp/app.py
+++ task3_form/FlaskApp/app.py
@@ -1,21 +1,69 @@
-from flask import Flask, render_template, request, redirect
-from forms import OrderForm
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+CHARACTERS = ('znak', 'znaki', 'znaków')
 orders = []
+
+
+def plural_form(n):
+    if n == 1:
+        plural = 0
+    else:
+        if n % 10 >= 2 and n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+            plural = 1
+        else:
+            plural = 2
+
+    return plural
+
+
+def length_validation(field, req=False, min_le=None, max_le=None):
+    field_length = (field and len(field)) or 0
+    if field_length == 0:
+        if req is True:
+            return "Nie zapomnij o tym polu."
+    elif min_le is not None and field_length < min_le:
+        return f'Co tak skromnie? Wpisz co najmniej \
+            {min_le} {CHARACTERS[plural_form(min_le)]}.'
+    elif max_le is not None and field_length > max_le:
+        return f'Nie przesadzasz z tą ilością? Wpisz maksymalnie \
+            {max_le} {CHARACTERS[plural_form(max_le)]}.'
+    else:
+        return False
 
 
 @app.route("/", methods=['GET', 'POST'])
 def show_page():
-    form = OrderForm(request.form, person='')
-    if request.method == 'POST' and form.validate():
-        person = form.person.data
-        food = form.food.data
+    if request.method == 'POST':
+        person = request.form['person']
+        food = request.form['food']
+
+        person_val_msg = length_validation(
+            person, req=True, min_le=3, max_le=64
+        )
+        food_val_msg = length_validation(
+            food, req=True, max_le=246
+        )
+
+        if person_val_msg or food_val_msg:
+            return render_template(
+                'index.html',
+                orders=orders,
+                person_val_msg=person_val_msg,
+                food_val_msg=food_val_msg
+            )
+
         orders.append([person, food])
-        return redirect("/")
-    return render_template('index.html', orders=orders, form=form)
+        return render_template(
+            'index.html',
+            orders=orders,
+        )
+
+    return render_template(
+        'index.html', orders=orders,
+    )
 
 
 if __name__ == "__main__":
